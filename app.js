1ff06f3afb468ad364c3eb474f1ed641db121260
subhro <subhrothecoder@gmail.com>
2020-06-14 21:34:43 +0530

    .

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const path=require('path');
 const express=require('express');
+const mongoose = require('mongoose');
 const webRoute=require('./routes/web');
 var app=express();
 
 //setting view engine
 app.set('view engine','ejs');
 
+app.use(express.urlencoded({extended:false}))
 //settinng up webRoutes for '/'
 app.use('/',webRoute);
 
@@ -13,5 +15,11 @@ app.use('/',webRoute);
 
 app.use('/public',express.static(path.join(__dirname,'public/')))
 
+//setting up mongoose
+
+mongoose.connect("mongodb+srv://subhro:sMdUFgQKsm92rjo9@cluster0-vzcwu.mongodb.net/books?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
 
 app.listen(5000);
--- routes/web.js
+++ routes/web.js
@@ -1,12 +1,42 @@
 const express = require('../node_modules/express');
+const Books=require("../models/books");
 var router=express.Router();
 
 //home route
-router.get('/',(request,response)=>{
-    response.render('index',{
-        title:'The Book App'
-    });
+router.get('/',async (request,response)=>{
+    var books=await Books.find();
+    //get all the books
+    
+    
+
+
+    response.render('index',{books:books});
 });
+
+router.get('/add',(request,response)=>{
+    response.render('add');
+});
+router.post('/add',(request,response)=>{
+    const book =new Books({
+        title:request.body.title,
+        author:request.body.author,
+        genre:request.body.genre,
+        pages:request.body.pages,
+        cost:request.body.cost,
+        image:request.body.image,
+        description:request.body.description
+    });
+    book.save()
+        .then(
+            response.redirect('/')
+        )
+        .catch(
+            (res)=>{
+                console.log(res);
+                response.redirect('/add');
+            }
+        );
+})
 
 
 module.exports=router
